94d36ca5bc10235f9921ca8a9281583ddfc0473f
Jerry Anselmi <jerry.anselmi@gmail.com>
2020-11-03 10:37:52 -0300

    Add Custom Errors
     - Exception is the base custom error
    
    Add Http error
    
     - BadRequestError
     - UnauthorizedError
     - PaymentRequiredError
     - ForbiddenError
     - NotFoundError
     - MethodNotAllowedError
     - ConflictError
     - UnsupportedMediaTypeError
     - UnprocessableEntityError
     - TooManyRequestsError
     - InternalServerError
     - ServiceUnavailableError

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,3 +8,70 @@ export namespace App {
         (options?: CreateAppConfig): Express.Application
     }
 }
+
+export namespace Helpers {
+    /**
+     * Help to export an Object
+     */
+    export interface Serializable<T> {
+        export(): T
+        /**
+         * Create an Json string from current Object
+         */
+        json(): string
+    }
+}
+
+export namespace Errors {
+    export interface BuilderBoxErrorProps {
+        /**
+         * Error code
+         * @type {string}
+         */
+        code: string
+
+        /**
+         * Http error status code
+         * @type {number}
+         */
+        status: number
+
+        /**
+         * Error trace
+         * @type {Array}
+         */
+        trace: Array<string>
+
+        /**
+         * Error context
+         * @type {Array}
+         */
+        context: Array<any>
+    }
+
+    export interface BuilderBoxErrorExportObject extends Object, BuilderBoxErrorProps {
+        /**
+         * Error class name
+         * @type{string}
+         */
+        name: string
+
+        /**
+         * Error message
+         * @type{string}
+         */
+        message: string
+    }
+
+    /**
+     * Interface to implement any BuilderBox error
+     */
+    export interface BuilderBoxError extends BuilderBoxErrorProps, Helpers.Serializable<BuilderBoxErrorExportObject> {
+        /**
+         * Validate if object is given error class name.
+         *
+         * @param name {string}
+         */
+        isA(name: string): boolean
+    }
+}
